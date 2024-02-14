faef7dbb579eb8afd1b7e07e03f4a8a1b55bb0c7
MASKUN2 <maskunflower2@naver.com>
2024-02-14 13:26:48 +0900

    스프링 시큐리티 초기설정으로 변경하기

--- src/main/java/com/maskun/springboot/web/IndexController.java
+++ src/main/java/com/maskun/springboot/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.maskun.springboot.web;
 
-import com.maskun.springboot.config.auth.dto.SessionUser;
 import com.maskun.springboot.service.posts.PostsService;
 import com.maskun.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +17,6 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user =(SessionUser) httpSession.getAttribute("user");
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
-        return "index";
-    }
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(Model model, @PathVariable Long id) {
         PostsResponseDto dto = postsService.findById(id);
